d1cc2e14430d98f31e1e4cc8d164549029c2b71e
Mo <mahmoudmonem0@gmail.com>
2022-11-15 00:35:57 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+const cityInput = document.querySelector("#search"),
+  API_KEY = "d6196ea5826327a1c37fc5fa26ccc5aa",
+  weatherDisplay = document.querySelector(".weather_display_section"),
+  DisplayCity = document.querySelector(".cityName"),
+  DisplayTemp = document.querySelector(".temp"),
+  DisplayTempShortDesc = document.querySelector(".temp_short_desc"),
+  DisplayHumidity = document.querySelector(".humidity"),
+  DisplayWindSpeed = document.querySelector(".wind_speed");
+
+const button = document.querySelector(".submit");
+
+button.addEventListener("click", function (e) {
+  getWeather(cityInput.value);
+  cityInput.value = "";
+  e.preventDefault();
+});
+
+function getWeather(city) {
+  const latLonUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${API_KEY}`;
+  DisplayCity.textContent = city;
+
+  fetch(latLonUrl).then((response) => {
+    response.json().then((data) => {
+      const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${data[0].lat}&lon=${data[0].lon}&appid=${API_KEY}`;
+      fetch(weatherURL).then((response) => {
+        response.json().then((data) => {
+          displayWeather(data);
+          console.log(data);
+        });
+      });
+    });
+  });
+}
+
+function displayWeather(data) {
+  weatherDisplay.classList.add("active");
+  DisplayTemp.innerHTML =
+    `<img id="wicon" src="http://openweathermap.org/img/w/${data.weather[0].icon}.png" alt="Weather icon">` +
+    Math.round(data.main.temp - 273.15) +
+    "°C";
+  DisplayTempShortDesc.textContent =
+    data.weather[0].description[0].toUpperCase() +
+    data.weather[0].description.slice(1);
+
+  DisplayHumidity.textContent = "Humidity: " + data.main.humidity + "%";
+  DisplayWindSpeed.textContent = "Wind Speed: " + data.wind.speed + "km/h";
+}
